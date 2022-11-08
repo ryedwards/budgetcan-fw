79d4c7c9c12acb117469630c582054eddf235d3b
Ryan Edwards <ryan.edwards@gmail.com>
2022-11-08 18:48:53 -0500

    Add board package that runs on the DevEBox H7 board
    https://stm32-base.org/boards/STM32H743VIT6-STM32H7XX-M.html
    Tested USB and LED functionality, did not test FDCAN.

---
+++ Portable/board_DevEBoxH7/Src/board.c
@@ -0,0 +1,213 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2022 Ryan Edwards
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "board.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "can.h"
+#include "led.h"
+
+LED_HandleTypeDef hled1;
+
+FDCAN_HandleTypeDef hfdcan1;
+FDCAN_HandleTypeDef hfdcan2;
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Supply configuration update enable
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 20;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/** Configure pins
+*/
+void MX_GPIO_Init(void)
+{
+ GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED1_Pin */
+  GPIO_InitStruct.Pin = LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
+}
+
+/** @brief Function to init any features specific to this board
+ *  @param None
+ *  @retval None
+ */
+void board_init(void)
+{
+  led_init(&hled1, LED1_GPIO_Port, LED1_Pin, LED_MODE_INACTIVE, LED_ACTIVE_LOW);
+}
+
+/** @brief Function to init all of the CAN channels this board supports
+ *  @param None
+ *  @retval None
+ */
+void board_can_init(void)
+{
+  can_init(&hfdcan1, FDCAN1);
+  can_init(&hfdcan2, FDCAN2);
+}
+
+/** @brief Function to assign the CAN HW pointers to the channel index in the USB handle
+ *  @param USBD_HandleTypeDef *hUSB - The handle for the USB where will will set up the CAN pointer
+ *  @retval None
+ */
+void board_usbd_gs_can_set_channel(USBD_HandleTypeDef *hUSB)
+{
+  USBD_GS_CAN_SetChannel(hUSB, 0, &hfdcan1);
+  USBD_GS_CAN_SetChannel(hUSB, 1, &hfdcan2);
+}
+
+/** @brief Function to periodically update any features on the board from the main task
+ *  @param None
+ *  @retval None
+ */
+void board_main_task_cb(void)
+{
+  /* update all the LEDs */
+ led_update(&hled1);
+}
+
+/** @brief Function called when the CAN is enabled for this channel
+ *  @param uint8_t channel - The CAN channel (0 based)
+ *  @retval None
+ */
+void board_on_can_enable_cb(uint8_t channel)
+{
+  led_set_active(&hled1);
+}
+
+/** @brief Function called when the CAN is disabled for this channel
+ *  @param uint8_t channel - The CAN channel (0 based)
+ *  @retval None
+ */
+void board_on_can_disable_cb(uint8_t channel)
+{
+  led_set_inactive(&hled1);
+}
+
+/** @brief Function called when a CAN frame is send on this channel
+ *  @param uint8_t channel - The CAN channel (0 based)
+ *  @retval None
+ */
+void board_on_can_tx_cb(uint8_t channel)
+{
+  led_indicate_rxtx(&hled1);
+}
+
+/** @brief Function called when a CAN frame is received on this channel
+ *  @param uint8_t channel - The CAN channel (0 based)
+ *  @retval None
+ */
+void board_on_can_rx_cb(uint8_t channel)
+{
+  led_indicate_rxtx(&hled1);
+}
+
+/** @brief Function called to set the CAN termination resistor ON of OFF
+ *  @param uint8_t channel - The CAN channel (0 based)
+ *  @param GPIO_PinState state - The requested state of the pin
+ *  @retval None
+ */
+void board_set_can_term(uint8_t channel, GPIO_PinState state)
+{
+}
+
+/** @brief Function called to get the state of the CAN termination resistor
+ *  @param uint8_t channel - The CAN channel (0 based)
+ *  @retval The current state of the CAN termination pin
+ */
+GPIO_PinState board_get_can_term(uint8_t channel)
+{
+  return 0;
+}
+
