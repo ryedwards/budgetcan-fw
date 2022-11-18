2ec59ed284856e1abc5790d29111a238583a8e2c
Ryan Edwards <ryan.edwards@gmail.com>
2022-11-17 23:51:33 -0500

    Update canablev2 with the correct I/O to enable CAN and LEDs
    Also included missing init for TIM2

--- Portable/board_canablev2/Inc/board.h
+++ Portable/board_canablev2/Inc/board.h
@@ -47,14 +47,14 @@ extern "C" {
 #define DFU_INTERFACE_STRING        (uint8_t*) "canablev2 DFU interface"
 
 #define FDCAN_SJW_INIT        1
-#define FDCAN_BRP_INIT        8
+#define FDCAN_BRP_INIT        10
 #define FDCAN_TS1_INIT        13
 #define FDCAN_TS2_INIT        2
 
-#define FDCAN_DATA_SJW_INIT   4
+#define FDCAN_DATA_SJW_INIT   1
 #define FDCAN_DATA_BRP_INIT   2
-#define FDCAN_DATA_TS1_INIT   15
-#define FDCAN_DATA_TS2_INIT   4
+#define FDCAN_DATA_TS1_INIT   14
+#define FDCAN_DATA_TS2_INIT   5
 
 #define FDCAN_CLOCK_DIV_INIT        FDCAN_CLOCK_DIV1
 #define FDCAN_FRAME_FMT_INIT        FDCAN_FRAME_FD_BRS
@@ -68,12 +68,18 @@ extern "C" {
 
 #define CANFD_SUPPORT     1
 #define CAN_NUM_CHANNELS  1
-#define CAN_CLOCK_SPEED   48000000
+#define CAN_CLOCK_SPEED   80000000
 
 #define QUEUE_SIZE_HOST_TO_DEV    32
 #define QUEUE_SIZE_DEV_TO_HOST    32
 
-#define LED1_Pin GPIO_PIN_8
+#define FDCAN1_3V3_EN_Pin GPIO_PIN_13
+#define FDCAN1_3V3_EN_GPIO_Port GPIOC
+#define FDCAN1_nSTANDBY_Pin GPIO_PIN_0
+#define FDCAN1_nSTANDBY_GPIO_Port GPIOA
+#define LED2_Pin GPIO_PIN_11
+#define LED2_GPIO_Port GPIOB
+#define LED1_Pin GPIO_PIN_15
 #define LED1_GPIO_Port GPIOA
 
 void SystemClock_Config(void);
--- Portable/board_canablev2/Src/board.c
+++ Portable/board_canablev2/Src/board.c
@@ -31,6 +31,7 @@ THE SOFTWARE.
 
 FDCAN_HandleTypeDef hfdcan1;
 LED_HandleTypeDef hled1;
+LED_HandleTypeDef hled2;
 
 extern USBD_GS_CAN_HandleTypeDef hGS_CAN;
 
@@ -45,7 +46,7 @@ void SystemClock_Config(void)
 
   /** Configure the main internal regulator output voltage
   */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -57,9 +58,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
-  RCC_OscInitStruct.PLL.PLLN = 20;
+  RCC_OscInitStruct.PLL.PLLN = 10;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -75,7 +76,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
@@ -87,19 +88,41 @@ void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-   /* GPIO Ports Clock Enable */
-   __HAL_RCC_GPIOA_CLK_ENABLE();
-   __HAL_RCC_GPIOB_CLK_ENABLE();
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(FDCAN1_3V3_EN_GPIO_Port, FDCAN1_3V3_EN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LED1_Pin */
-  GPIO_InitStruct.Pin = LED1_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, FDCAN1_nSTANDBY_Pin|LED1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : FDCAN1_3V3_EN_Pin */
+  GPIO_InitStruct.Pin = FDCAN1_3V3_EN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(FDCAN1_3V3_EN_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : FDCAN1_nSTANDBY_Pin LED1_Pin */
+  GPIO_InitStruct.Pin = FDCAN1_nSTANDBY_Pin|LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED2_Pin */
+  GPIO_InitStruct.Pin = LED2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
+
 }
 
 /** @brief Function to init all of the LEDs that this board supports
@@ -108,9 +131,20 @@ void MX_GPIO_Init(void)
  */
 void main_init_cb(void)
 {
+  /* carry over the LED blink from original firmware */
+  for (uint8_t i=0; i<10; i++)
+	{
+		HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+		HAL_Delay(50);
+		HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+	}
+
+  HAL_GPIO_WritePin(FDCAN1_nSTANDBY_GPIO_Port, FDCAN1_nSTANDBY_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(FDCAN1_3V3_EN_GPIO_Port, FDCAN1_3V3_EN_Pin, GPIO_PIN_SET); 
   hGS_CAN.channels[0] = &hfdcan1;
   can_init(hGS_CAN.channels[0], FDCAN1);
-  led_init(&hled1, LED1_GPIO_Port, LED1_Pin, LED_MODE_INACTIVE, LED_ACTIVE_HIGH);
+  led_init(&hled1, LED1_GPIO_Port, LED1_Pin, LED_MODE_ACTIVE, LED_ACTIVE_HIGH);
+  led_init(&hled2, LED2_GPIO_Port, LED2_Pin, LED_MODE_INACTIVE, LED_ACTIVE_HIGH);
 }
 
 /** @brief Function to periodically update any features on the board from the main task
@@ -121,6 +155,7 @@ void main_task_cb(void)
 {
   /* update all the LEDs */
   led_update(&hled1);
+  led_update(&hled2);
 }
 
 /** @brief Function called when a CAN frame is send on this channel
@@ -131,7 +166,7 @@ void can_on_tx_cb(uint8_t channel, struct GS_HOST_FRAME *frame)
 {
   UNUSED(channel);
   UNUSED(frame);
-  led_indicate_rxtx(&hled1);
+  led_indicate_rxtx(&hled2);
 }
 
 /** @brief Function called when a CAN frame is received on this channel
@@ -142,5 +177,5 @@ void can_on_rx_cb(uint8_t channel, struct GS_HOST_FRAME *frame)
 {
   UNUSED(channel);
   UNUSED(frame);
-  led_indicate_rxtx(&hled1);
+  led_indicate_rxtx(&hled2);
 }
