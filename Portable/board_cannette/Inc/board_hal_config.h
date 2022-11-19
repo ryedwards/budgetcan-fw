5689a62741fd73ea07c9d5dd0f9887d1bcb2e4ba
Ryan Edwards <ryan.edwards@gmail.com>
2022-11-19 10:11:48 -0500

    Initial commit of canette test
    Trial to implement the board features

---
+++ Portable/board_cannette/Inc/board_hal_config.h
@@ -0,0 +1,37 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32_hal_config.h
+  * @brief   This file contains the define to select the correct
+  *          HAL for this board
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32_HAL_CONFIG_H__
+#define __STM32_HAL_CONFIG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__STM32_HAL_CONFIG_H__ */
+
