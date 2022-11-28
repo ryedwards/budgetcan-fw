b00fae888e7454809b9ce74ec9629b113964d0c4
Ryan Edwards <ryan.edwards@gmail.com>
2022-11-27 22:33:43 -0500

    Add the identify callback and remove redundant headers

--- Portable/board_cannette/Src/board.c
+++ Portable/board_cannette/Src/board.c
@@ -43,160 +43,146 @@ extern USBD_GS_CAN_HandleTypeDef hGS_CAN;
   */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	* in the RCC_OscInitTypeDef structure.
+	*/
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	*/
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+								  |RCC_CLOCKTYPE_PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
 
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /** Configure pins
 */
 void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, nSI86_EN_Pin|CAN_nSTANDBY_Pin|DCDC_EN_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOC, nSI86_EN_Pin|CAN_nSTANDBY_Pin|DCDC_EN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_TX_Pin|LED_RX_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOA, LED_TX_Pin|LED_RX_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : nSI86_EN_Pin CAN_nSTANDBY_Pin DCDC_EN_Pin */
-  GPIO_InitStruct.Pin = nSI86_EN_Pin|CAN_nSTANDBY_Pin|DCDC_EN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	/*Configure GPIO pins : nSI86_EN_Pin CAN_nSTANDBY_Pin DCDC_EN_Pin */
+	GPIO_InitStruct.Pin = nSI86_EN_Pin|CAN_nSTANDBY_Pin|DCDC_EN_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : SENSE_5V_Pin */
-  GPIO_InitStruct.Pin = SENSE_5V_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(SENSE_5V_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : SENSE_5V_Pin */
+	GPIO_InitStruct.Pin = SENSE_5V_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(SENSE_5V_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LED_TX_Pin LED_RX_Pin */
-  GPIO_InitStruct.Pin = LED_TX_Pin|LED_RX_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	/*Configure GPIO pins : LED_TX_Pin LED_RX_Pin */
+	GPIO_InitStruct.Pin = LED_TX_Pin|LED_RX_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 
-/** @brief Function to init all of the LEDs that this board supports
- *  @param None
- *  @retval None
- */
 void main_init_cb(void)
 {
-  /* carry over the LED blink from original firmware */
-  for (uint8_t i=0; i<10; i++)
+	/* carry over the LED blink from original firmware */
+	for (uint8_t i=0; i<10; i++)
 	{
 		HAL_GPIO_TogglePin(LED_RX_GPIO_Port, LED_RX_Pin);
 		HAL_Delay(50);
 		HAL_GPIO_TogglePin(LED_TX_GPIO_Port, LED_TX_Pin);
 	}
-  
-  HAL_GPIO_WritePin(CAN_nSTANDBY_GPIO_Port, CAN_nSTANDBY_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(nSI86_EN_GPIO_Port, nSI86_EN_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(DCDC_EN_GPIO_Port, DCDC_EN_Pin, GPIO_PIN_SET);
 
-  hGS_CAN.channels[0] = &hcan;
-  can_init(hGS_CAN.channels[0], CAN);
+	HAL_GPIO_WritePin(CAN_nSTANDBY_GPIO_Port, CAN_nSTANDBY_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(nSI86_EN_GPIO_Port,	  nSI86_EN_Pin,		GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DCDC_EN_GPIO_Port,	  DCDC_EN_Pin,		GPIO_PIN_SET);
 
-  led_init(&hledrx, LED_RX_GPIO_Port, LED_RX_Pin, LED_MODE_INACTIVE, LED_ACTIVE_LOW);
-  led_init(&hledtx, LED_TX_GPIO_Port, LED_TX_Pin, LED_MODE_ACTIVE, LED_ACTIVE_LOW);
+	hGS_CAN.channels[0] = &hcan;
+	can_init(hGS_CAN.channels[0], CAN);
+
+	led_init(&hledrx, LED_RX_GPIO_Port, LED_RX_Pin, LED_MODE_INACTIVE, LED_ACTIVE_LOW);
+	led_init(&hledtx, LED_TX_GPIO_Port, LED_TX_Pin, LED_MODE_ACTIVE,   LED_ACTIVE_LOW);
 }
 
-/** @brief Function to periodically update any features on the board from the main task
- *  @param None
- *  @retval None
- */
 void main_task_cb(void)
 {
-  /* update all the LEDs */
-  led_update(&hledrx);
-  led_update(&hledtx);
+	/* update all the LEDs */
+	led_update(&hledrx);
+	led_update(&hledtx);
 
-  if (HAL_GPIO_ReadPin(SENSE_5V_GPIO_Port, SENSE_5V_Pin) == 0) {
-    /* Do something */
-  }
+	if (HAL_GPIO_ReadPin(SENSE_5V_GPIO_Port, SENSE_5V_Pin) == 0) {
+		/* Do something */
+	}
 }
 
-/** @brief Function called when the CAN is enabled for this channel
- *  @param uint8_t channel - The CAN channel (0 based)
- *  @retval None
- */
 void can_on_enable_cb(uint8_t channel)
 {
-  UNUSED(channel);
-  led_set_inactive(&hledtx);
-  led_set_active(&hledrx);
+	UNUSED(channel);
+	led_set_inactive(&hledtx);
+	led_set_active(&hledrx);
 }
 
-/** @brief Function called when the CAN is disabled for this channel
- *  @param uint8_t channel - The CAN channel (0 based)
- *  @retval None
- */
 void can_on_disable_cb(uint8_t channel)
 {
-  UNUSED(channel);
-  led_set_inactive(&hledrx);
-  led_set_active(&hledtx);
+	UNUSED(channel);
+	led_set_inactive(&hledrx);
+	led_set_active(&hledtx);
 }
 
-/** @brief Function called when a CAN frame is send on this channel
- *  @param uint8_t channel - The CAN channel (0 based)
- *  @retval None
- */
 void can_on_tx_cb(uint8_t channel, struct gs_host_frame *frame)
 {
-  UNUSED(channel);
-  UNUSED(frame);
-  led_indicate_rxtx(&hledtx);
+	UNUSED(channel);
+	UNUSED(frame);
+	led_indicate_rxtx(&hledtx);
 }
 
-/** @brief Function called when a CAN frame is received on this channel
- *  @param uint8_t channel - The CAN channel (0 based)
- *  @retval None
- */
 void can_on_rx_cb(uint8_t channel, struct gs_host_frame *frame)
 {
-  UNUSED(channel);
-  UNUSED(frame);
-  led_indicate_rxtx(&hledrx);
+	UNUSED(channel);
+	UNUSED(frame);
+	led_indicate_rxtx(&hledrx);
+}
+
+void can_identify_cb(uint32_t do_identify)
+{
+	if (do_identify) {
+		led_blink_start(&hledrx, 250);
+	}
+	else {
+		led_blink_stop(&hledrx);
+	}
 }
